0440f5bdd4f862b0bd0984beed466072023fc7d4
Nguyễn Đình Phúc Thành <163141155+00rei000@users.noreply.github.com>
2024-10-28 21:34:27 +0700

    Update Aims.java

--- lab02/AimsProjectCode/Aims.java
+++ lab02/AimsProjectCode/Aims.java
@@ -5,14 +5,13 @@ public class Aims {
 
 		Cart order = new Cart();
 
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("Doraemon the movie", "Anime", "Zen Soichiro", 90, 25.50f);
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("One piece", "Anime", "Oda Eiichirou", 90, 25.50f);
 		order.addDigitalVideoDisc(dvd1);
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Spiderman no way home", "Science Fiction", "Jon Watts", 90,
-				30.15f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Grand Blue", "Comedy", "Kenji Inoue", 90, 20.15f);
 		order.addDigitalVideoDisc(dvd2);
 
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Naruto", "Anime", 21.30f);
-		DigitalVideoDisc dvd4 = new DigitalVideoDisc("Diary of a Wimpy Kid", "Comedy", "Thor Freudenthal", 40.50f);
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("Bleach", "Anime", 22.50f);
 		order.addDigitalVideoDisc(dvd3);
 		order.addDigitalVideoDisc(dvd4);
 		order.removeDigitalVideoDisc(dvd2);
